969e3e81d6b4b16a7b679ae90e61b755c329150c
Data Engineering Blogpost <56186002+karthik2123@users.noreply.github.com>
2023-11-26 07:43:31 +0530

    Update IncreamentalLoad.py

--- IncreamentalLoad.py
+++ IncreamentalLoad.py
@@ -1,33 +1,17 @@
-import snowflake.connector
-
-sfconn = snowflake.connector.connect(
-              user = "karthik",
-            password = "Karthiksara@2123",
-            account  = 'vpb37715.us-east-1',
-            )
-sfconn = sfconn
-sf = sfconn.cursor()
-use_warehouse_command = f"USE WAREHOUSE sample_wh;"
-sf.execute(use_warehouse_command)
-
-database_create = f"USE DATABASE TSeries"
-sf.execute(database_create)
-        
-
-database_create = "USE SCHEMA PUBLIC"
-sf.execute(database_create)
-use_warehouse_command = f'SELECT max("publishedAt") FROM "Tseries_table";'
-sf.execute(use_warehouse_command)
-results = sf.fetchall()
-a_week = results[0][0]
-from script4 import FullLoadDataToS3
-from Script2 import RawToProcess
-from Script3 import SnowFlakeDataIngest
-import datetime
-
-if __name__ == "__main__":
-
-        #Youtube API Key 
+import boto3
+import json
+def Increamental_Load():
+    s3_client = boto3.client('s3')
+    # self.s3_client = s3_client
+    response = s3_client.get_object(Bucket="snowpipebucket2521", Key="Bookmark")
+    # Deserialize the JSON object to a dictionary
+    metadata = json.loads(response['Body'].read().decode('utf-8'))
+    a_week = metadata['last_update_date']
+    print(a_week)
+    from Load import FullLoadDataToS3
+    from Preprocess import RawToProcess
+    from Transform import SnowFlakeDataIngest
+    import datetime
     api_key = "AIzaSyCgxCRumhmu7F6Z2NDXA4vShDSkZ7ba6lY"
 
     """
@@ -43,28 +27,34 @@ if __name__ == "__main__":
     FullLoadDataToS3_obj = FullLoadDataToS3(api_key,channel_id)
     Bucket_name = "my-bucket-2521"
     now = datetime.datetime.now()
-    input_format="%Y-%m-%dT%H:%M:%SZ"
-    output_format="%Y-%m-%d %H:%M:%S.%f"
-    date_object = datetime.datetime.strptime(a_week, input_format)
-
+    # input_format="%Y-%m-%dT%H:%M:%SZ"
+    # output_format="%Y-%m-%d %H:%M:%S.%f"
+    # date_object = datetime.datetime.strptime(a_week, input_format)
+    date_object = a_week
+    a_week = datetime.datetime.strptime(date_object, '%Y-%m-%d %H:%M:%S.%f')
+    
     # Format the datetime object to the desired format
-    a_week = date_object.strftime(output_format)
-    a_week = datetime.datetime.strptime(a_week, output_format)
+    # a_week = date_object.strftime(output_format)
+    # a_week = datetime.datetime.strptime(a_week, output_format)
     
     # a_week = datetime.datetime.strptime(a_week, output_format)
     
 
-    raw_file_name = FullLoadDataToS3_obj.run(End_date = a_week , Bucket_name = Bucket_name)
-    raw_bucket_name = "my-bucket-2521"
-    clean_bucket_name = "cleanbucket2521"
-    # raw_file_name = "put_data_from2023-9-29-7_to_2023-10-6-7"
-
-    RawToProcess = RawToProcess(raw_bucket_name , clean_bucket_name)
-    RawToProcess.run(raw_file_name = raw_file_name)
-
-    Clean_Bucket_name ="cleanbucket2521"
-    start_date = a_week
-    end_date = now
-
-    SnowFlakeDataIngestObj = SnowFlakeDataIngest() 
-    SnowFlakeDataIngestObj.run(Clean_Bucket_name , start_date , end_date)
+    raw_file_name = FullLoadDataToS3_obj.run(End_Date = a_week , Bucket_Name = Bucket_name)
+    print(raw_file_name[1])
+    if raw_file_name[1]!=0:
+        raw_bucket_name = "my-bucket-2521"
+        clean_bucket_name = "cleanbucket2521"
+        # raw_file_name = "put_data_from2023-9-29-7_to_2023-10-6-7"
+        
+        RawToProcess = RawToProcess(raw_bucket_name , clean_bucket_name)
+        RawToProcess.run(raw_file_name = raw_file_name[0])
+        
+        Clean_Bucket_name ="cleanbucket2521"
+        start_date = a_week
+        end_date = now
+        
+        SnowFlakeDataIngestObj = SnowFlakeDataIngest() 
+        SnowFlakeDataIngestObj.run(Clean_Bucket_name , start_date , end_date)
+    else:
+        print("Data execution stoped due no videos")
